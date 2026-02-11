dcc5d3222c7535c444d39d001417224b9b95aceb
Rifthan Fathir <143585916+Fathir2001@users.noreply.github.com>
2026-02-11 23:20:52 +0530

    Revert "Develop (#6)"
    This reverts commit 6ae34a2494422c071e0f5e38facdef572c8738d1.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  // Optimize performance
-  poweredByHeader: false,
-  compress: true,
-  // Reduce JavaScript execution time
-  productionBrowserSourceMaps: false,
 };
 
 module.exports = nextConfig;
